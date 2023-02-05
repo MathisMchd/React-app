16559c5bf63c3f729682a1e9f82a00009292cf73
MathisMchd <michenaudmathis@gmail.com>
2023-02-05 22:10:47 +0100

    navBar logic

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
-const intitialState = {
+const initialState = {
     chat: false,
     cart: false,
     userProfile: false,
@@ -13,12 +13,20 @@ const intitialState = {
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
 
+    const [isClicked, setIsClicked] = useState(initialState);
+
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true });
+    }
 
     return (
         <StateContext.Provider
             value={{
                 activeMenu,
                 setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick
             }}
         >
             {children}
